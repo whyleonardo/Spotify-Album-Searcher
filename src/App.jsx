d11f03d2ec3066de6f2bd4cb615b22907d4f52fb
whyleonardo <pleasefloyd73@gmail.com>
2022-07-04 23:08:52 -0300

    feat: layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { SpotifyLogo } from 'phosphor-react'
 
 const CLIENT_ID = 'd01c25e7230c459c809567db676e0f83'
 const CLIENT_SECRET = 'd96f38d7df61439d9e2696d85fb3ad42'
@@ -7,6 +8,7 @@ export const App = () => {
   const [searchInput, setSearchInput] = useState('')
   const [accessToken, setAccessToken] = useState('')
   const [albums, setAlbums] = useState([])
+  const [artistName, setArtistName] = useState('')
 
   useEffect(() => {
     // API Access Token
@@ -39,7 +41,10 @@ export const App = () => {
     )
       .then(response => response.json())
       .then(data => {
-        return data.artists.items[0].id
+        const artistAlbums = data.artists.items[0].id
+        const artistName = data.artists.items[0].name
+        setArtistName(artistName)
+        return artistAlbums
       })
 
     // Get request with Artist ID grab all the albums from that artists
@@ -49,48 +54,65 @@ export const App = () => {
     )
       .then(response => response.json())
       .then(data => {
-        console.log(data.items)
         setAlbums(data.items)
       })
 
     setSearchInput('')
   }
-
+  // setArtist(data.artist.name)
   return (
     <div className="flex flex-col justify-center px-4 py-8">
-      <div className="">
-        <h1 className="text-3xl uppercase">Spotify Album Search</h1>
+      <div>
+        <h1 className="text-2xl text-center text-brand-900 font-bold uppercase md:text-4xl">
+          Spotify Album Search
+        </h1>
       </div>
-      <div className="flex mt-2 mb-4 flex-col gap-1">
+      <div className="flex mt-2 mb-4 flex-col gap-2 items-center">
         <input
-          className="w-full placeholder:text-center border rounded border-red-500 outline-none p-3"
+          className="w-full placeholder:text-center placeholder:opacity-40 border rounded border-brand-700 outline-none p-3 md:w-2/4"
           placeholder="Search for an artist"
           value={searchInput}
+          onKeyDown={event => (event.key == 'Enter' ? handleSearchAlbum() : '')}
           onChange={event => setSearchInput(event.target.value)}
         />
         <button
-          className="w-full uppercase font-bold border rounded bg-red-500 outline-none p-3"
+          className="w-full text-white uppercase font-bold rounded bg-brand-700 outline-none p-3 md:w-1/4"
           onClick={handleSearchAlbum}
         >
-          Search
+          Search Albums
         </button>
       </div>
-      <div>
+      <div className="mb-6 text-center">
+        {' '}
+        <strong className="text-2xl text-brand-900  font-bold uppercase md:text-3xl">
+          {artistName}
+        </strong>
+      </div>
+      <div className="grid gap-6 text-center text-white md:grid md:grid-cols-4 md:grid-rows-none">
         {albums.map(album => {
           return (
-            <div className="" key={album.id}>
-              <div>
+            <div className="bg-brand-900 p-4 rounded" key={album.id}>
+              <div className="grid gap-2">
                 <img src={album.images[0].url} />
                 <span className="block uppercase font-bold text-center">
                   {album.name}
                 </span>
-                <span className="block">
-                  <span className="font-bold ">Year: </span>
-                  {album.release_date.slice(0, 4)}
-                </span>
-                <a href={album.external_urls.spotify} target="_blank">
-                  Abrir no Spotify
-                </a>
+
+                <div className="flex flex-col items-center">
+                  {' '}
+                  <span>
+                    <span className="font-bold ">Year: </span>
+                    {album.release_date.slice(0, 4)}
+                  </span>
+                  <a
+                    className="flex items-center justify-center gap-1 hover:underline"
+                    href={album.external_urls.spotify}
+                    target="_blank"
+                  >
+                    <SpotifyLogo size={24} />
+                    Listen on Spotify
+                  </a>
+                </div>
               </div>
             </div>
           )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html,jsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        brand: {
+          100: '#A4B494',
+          300: '#BEC5AD',
+          500: '#3B5249',
+          700: '#519872',
+          900: '#34252F'
+        }
+      }
+    }
   },
   plugins: []
 }
